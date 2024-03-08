e743e4f0d361331fe5a2639df5bb74d3038f5c51
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-03-08 11:47:42 +0100

    feat: work on demo

--- demo/astro.config.ts
+++ demo/astro.config.ts
@@ -1,6 +1,8 @@
 import i18n from "@astrolicious/i18n";
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
@@ -9,6 +11,10 @@ export default defineConfig({
 		i18n({
 			defaultLocale: "en",
 			locales: ["en", "fr"],
+			client: {
+				data: true
+			}
 		}),
+		tailwind(),
 	],
 });
