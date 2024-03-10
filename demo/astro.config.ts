bdc9036280f2bff6516d4907fb8f541887de44fb
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-03-10 16:43:25 +0100

    fix: typing issue

--- demo/astro.config.ts
+++ demo/astro.config.ts
@@ -6,8 +6,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
-		// TODO: fix typing error (in AIK?)
-		// @ts-ignore
 		i18n({
 			defaultLocale: "en",
 			locales: ["en", "fr"],
