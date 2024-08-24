87ed904ca8d06096c54ba8d9415d6624f03d7467
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-08-24 11:34:04 +0200

    fix: types for astro >=4.14.0 (#33)

---
+++ .changeset/wicked-rabbits-wash.md
@@ -0,0 +1,5 @@
+---
+"@astrolicious/i18n": minor
+---
+
+Upgrades some dependencies to fix an issue with Astro >= 4.14.0. The package now requires Astro >= 4.12.0
--- playground/src/env.d.ts
+++ playground/src/env.d.ts
@@ -1,4 +1,3 @@
 /// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
 /// <reference types="../.astro/astro-i18n.d.ts" />
 /// <reference types="../.astro/i18next.d.ts" />
